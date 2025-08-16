7e1e37ae131902e056546c0d9eccac1b90feec11
bhumikamourya <bhumikamourya275@gmail.com>
2025-08-16 20:27:19 +0530

    final code

---
+++ backend/schemas/UserSchema.js
@@ -0,0 +1,25 @@
+const bcrypt = require("bcryptjs");
+const { Schema } = require("mongoose");
+const UserSchema = new Schema({
+     email: {
+    type: String,
+    required: [true, "Your email address is required"],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, "Your username is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Your password is required"],
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+UserSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 12);
+});
+module.exports = {UserSchema};
